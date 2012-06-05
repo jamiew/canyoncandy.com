fa16a4df57eb20d4e8dc34e4240c06f144678faf
Jamie Wilkinson <jamie@jamiedubs.com>
2012-06-05 16:30:37 -0700

    It's a Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Canyoncandy::Application.config.secret_token = 'd0a626f62001c567bf50cd281db067f64def3929ae21b4a65557a450688c7f57f14b0a89f14e538f59324bb74e54ea134187bd37132f77fa17c5e33538c0afc5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Canyoncandy::Application.config.session_store :cookie_store, :key => '_canyoncandy_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Canyoncandy::Application.config.session_store :active_record_store
